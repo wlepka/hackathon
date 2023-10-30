e5a38236ca9772bb68a2776506d9f6e1c1954358
Lim0ox <d.bahyt2015@gmail.com>
2023-10-30 19:57:38 +0600

    fixes

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ import requests
 #     print('Status:', response.status_code)
 #     print('Error:', response.text)
 
-TELEGRAM_TOKEN = '6694214287:AAGpTSElJiLJbMLPf-Jp7C9cvnrqmjlBGwA'
+TELEGRAM_TOKEN = '6425414932:AAE3p4gNl_eJqPc3D9FWWxtVmP1sDE6NiSo'
 # chatpdf = chatpdf.ChatPDF('sec_BBiFx587OnD7EOedplwQIpfPrjqZLp34')
 # chatpdf.load_pdf('https://drive.google.com/file/d/1-O8d_RjRsRTQg4v-Jqb39SQy9cZXfIU3/view?usp=sharing')
 
@@ -49,25 +49,28 @@ def button(update: Update, context: CallbackContext):
     else:
         context.bot.send_message(
             chat_id=user_id,
-            text="What would you like to do?",
+            text="What would you like to do1?",
             reply_markup=ReplyKeyboardMarkup([["Ask a Question", "Book"]], one_time_keyboard=True)
         )
 
 def grade_choice(update: Update, context: CallbackContext):
-    print('in grade choice')
     user_id = update.effective_user.id
     grade = update.message.text
-
-    user_choices[user_id] += " Grade " + grade
+    
+    user_choices[user_id] = grade
+    context.bot.send_message(
+        chat_id=user_id,
+        text = "in grade choice"
+    )
 
     context.bot.send_message(
         chat_id=user_id,
-        text="What would you like to do?",
+        text="What would you like to do2?",
         reply_markup=ReplyKeyboardMarkup([["Ask a Question", "Book"]], one_time_keyboard=True)
     )
 
 def option_choice(update: Update, context: CallbackContext):
-    print('in option choice')
+
     user_id = update.effective_user.id
     option = update.message.text
 
@@ -90,10 +93,6 @@ def option_choice(update: Update, context: CallbackContext):
             chat_id=user_id,
             text="Invalid option. Please choose 'Ask a Question' or 'Book'."
         )
-
-def get_pdf_answer(question):
-    # Assuming chatpdf can provide answers to questions
-    return chatpdf.get_response(question)
 
 def handle_user_question(update: Update, context: CallbackContext):
     user_id = update.effective_user.id
@@ -130,21 +129,27 @@ def handle_user_question(update: Update, context: CallbackContext):
     )
 
 
+# ...
+
+# Separate message handlers for button choice and grade choice
+button_handler = MessageHandler(Filters.regex(r'^(CompSci|Physics|Math)$'), button)
+grade_handler = MessageHandler(Filters.regex(r'^(7|8|9|10|11|12)$'), grade_choice)
+
+# ...
+
 if __name__ == '__main__':
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
     dp = updater.dispatcher
 
     start_handler = CommandHandler('start', start)
-    button_handler = MessageHandler(Filters.text & ~Filters.command, button)
-    grade_handler = MessageHandler(Filters.text & ~Filters.command, grade_choice)
-    option_handler = MessageHandler(Filters.text & ~Filters.command, option_choice)
+    option_handler = MessageHandler(Filters.regex(r'^(Ask a Question|Book)$'), option_choice)
     question_handler = MessageHandler(Filters.text & ~Filters.command, handle_user_question)
 
     dp.add_handler(start_handler)
-    dp.add_handler(button_handler)
-    dp.add_handler(grade_handler)
-    dp.add_handler(option_handler)
-    dp.add_handler(question_handler)
+    dp.add_handler(button_handler)  # Handles subject choices (e.g., CompSci, Physics, Math)
+    dp.add_handler(grade_handler)   # Handles grade choices (e.g., 7, 8, 9, 10, 11, 12)
+    dp.add_handler(option_handler)  # Handles "Ask a Question" and "Book" options
+    dp.add_handler(question_handler) # Handles user questions
 
     updater.start_polling()
     updater.idle()
